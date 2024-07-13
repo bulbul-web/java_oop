0c305261114dbbb246e0960f30d92abbe66f53fe
bulbul <sakarnirakar123@gmail.com>
2024-07-13 09:22:07 +0600

    restore files

---
+++ Second.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Second{
+    public static void main(String[] args){
+        int a = 10;
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    Scanner myObj1 = new Scanner(System.in);
+    System.out.println("Enter username");
+
+    String userName = myObj1.nextLine();
+    System.out.println("Username is: " + userName + a);
+    }
+}
